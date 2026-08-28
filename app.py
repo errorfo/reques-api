ca301cb1788d7e1e2b365c79c4112b1895cbc84d
OVAIS AHMAD THOKER <110771726+errorfo@users.noreply.github.com>
2026-08-28 21:54:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,24 +18,4 @@ def webhook():
     return jsonify({
         "success": True,
         "received": data
-    }), 200from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({
-        "status": "online",
-        "message": "Webhook API is running"
-    })
-
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    data = request.get_json(silent=True)
-
-    return jsonify({
-        "success": True,
-        "received": data
     }), 200
